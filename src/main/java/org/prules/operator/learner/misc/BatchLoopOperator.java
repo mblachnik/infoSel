dbf089ad6712aa8370d34a2693ea83211aa76ceb
FiloMortis <pawel.wanot@gmail.com>
2019-10-17 19:04:34 +0200

    Updated code of Batch Loop Operator

--- src/main/java/org/prules/operator/learner/misc/BatchLoopOperator.java
+++ src/main/java/org/prules/operator/learner/misc/BatchLoopOperator.java
@@ -29,29 +29,62 @@ import java.util.Map.Entry;
  * batch value are delivered to the input subprocess where a prediction model is trained.
  * As an output the operator returns a local ensemble model with models defined for each batch value, where batch is defined by a pair of prototypes.
  *
- * @author Marcin
+ * @author Marcin, Paweł
  */
 public class BatchLoopOperator extends OperatorChain {
+    //<editor-fold desc="Static data" defaultState="collapsed" >
+    private static final String PORT_INPUT_EXAMPLE = "example set";
+    private static final String PORT_INPUT_MODEL = "prototype model";
+    private static final String PORT_INNER_INPUT_EXAMPLE = "example set";
+    private static final String PORT_INNER_INPUT_MODEL = "prediction model";
+    private static final String PORT_OUTPUT_MODEL = "prediction model";
+    //</editor-fold>
+
+    //<editor-fold desc="Private fields" defaultState="collapsed" >
     /**
      * Training data returned by the NearestPrototypesOperator.
      */
-    private final InputPort exampleSetInputPort = getInputPorts().createPort("example Set", ExampleSet.class);
+    private final InputPort exampleSetInputPort = getInputPorts().createPort(PORT_INPUT_EXAMPLE, ExampleSet.class);
     /**
      * PrototypesEnsembleModel returned by the NearestPrototypesOperator
      */
-    private final InputPort prototypesEnsemble = getInputPorts().createPort("prototype model", PrototypesEnsembleModel.class);
+    private final InputPort prototypesEnsemble = getInputPorts().createPort(PORT_INPUT_MODEL, PrototypesEnsembleModel.class);
     /**
-     * training data delivered to the subProcess
+     * Training data delivered to the subProcess
      */
-    private final OutputPort exampleInnerSourcePort = getSubprocess(0).getInnerSources().createPort("example Set");
+    private final OutputPort exampleInnerSourcePort = getSubprocess(0).getInnerSources().createPort(PORT_INNER_INPUT_EXAMPLE);
     /**
      * PredictionModel obtained after training the prediction model
      */
-    private final InputPort predictionModelInnerSourcePort = getSubprocess(0).getInnerSinks().createPort("prediction model", PredictionModel.class);
+    private final InputPort predictionModelInnerSourcePort = getSubprocess(0).getInnerSinks().createPort(PORT_INNER_INPUT_MODEL, PredictionModel.class);
     /**
-     * THe final prediction model ensemble
+     * The final prediction model ensemble
      */
-    private final OutputPort finalModelOutputPort = getOutputPorts().createPort("prediction model");
+    private final OutputPort finalModelOutputPort = getOutputPorts().createPort(PORT_OUTPUT_MODEL);
+    /**
+     * input example set
+     */
+    private ExampleSet exampleSet;
+    /**
+     * Input model
+     */
+    private PrototypesEnsembleModel inputModel;
+    /**
+     * List of attributes
+     */
+    private Attribute attr;
+
+    /**
+     * Map of pair Id to model
+     */
+    private Map<Long, PredictionModel> modelsMap;//Map which cantons list of elements which belong to given batch
+    /**
+     * Map of pair Id to Data Index
+     */
+    private Map<Long, IDataIndex> pairsMap;
+    //</editor-fold>
+
+    //<editor-fold desc="Constructor" defaultState="collapsed" >
 
     /**
      * Constructor for the BatchLoopOperator
@@ -63,28 +96,38 @@ public class BatchLoopOperator extends OperatorChain {
         getTransformer().addPassThroughRule(exampleSetInputPort, exampleInnerSourcePort);
         getTransformer().addGenerationRule(finalModelOutputPort, PrototypesEnsemblePredictionModel.class);
     }
+    //</editor-fold>
+
+    // <editor-fold desc="Set up stage" defaultState="collapsed" >
 
     /**
-     * Main computation method. It takes input example set, generates subsets based
-     * on batch attribute and in a loop each batch is delivered to the subProcess
-     * After each subProcess execution all prediction models are collected in a map,
-     * and returned as a prediction model.
+     * Sets up configuration variables for process computation
      *
      * @throws OperatorException
      */
-    @Override
-    public void doWork() throws OperatorException {
-        Map<Long, PredictionModel> modelsMap;
-        ExampleSet exampleSet = exampleSetInputPort.getData(ExampleSet.class);
-        PrototypesEnsembleModel inputModel = prototypesEnsemble.getData(PrototypesEnsembleModel.class);
-        Attribute attr = exampleSet.getAttributes().findRoleBySpecialName(Attributes.BATCH_NAME).getAttribute();
-        //Map which cantons list of elements which belong to given batch
-        Map<Long, IDataIndex> pairsMap = new HashMap<>();
-        //Get all possible pairs and samples which belong to given pair
-        IDataIndex idx;
+    private void setup() throws OperatorException {
+        //Get example set
+        exampleSet = exampleSetInputPort.getData(ExampleSet.class);
+        //Get model
+        inputModel = prototypesEnsemble.getData(PrototypesEnsembleModel.class);
+        //Get attributes
+        attr = exampleSet.getAttributes().findRoleBySpecialName(Attributes.BATCH_NAME).getAttribute();
+        //Initialize maps
+        modelsMap = new HashMap<>();
+        pairsMap = new HashMap<>();
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Compute stage" defaultState="collapsed" >
+
+    /**
+     * Gets all possible pairs and samples which belong to given pair
+     */
+    private void mapPairs() {
         int exampleIndex = 0;
-        for (Example e : exampleSet) {
-            double pairId = e.getValue(attr);
+        IDataIndex idx;
+        for (Example example : exampleSet) {
+            double pairId = example.getValue(attr);
             if (pairsMap.containsKey((long) pairId)) {
                 idx = pairsMap.get((long) pairId);
             } else {
@@ -95,7 +138,15 @@ public class BatchLoopOperator extends OperatorChain {
             idx.set(exampleIndex, true);
             exampleIndex++;
         }
-        modelsMap = new HashMap<>();
+    }
+
+    /**
+     * Train experts in inner sub process
+     *
+     * @throws OperatorException
+     */
+    private void trainExperts() throws OperatorException {
+        IDataIndex idx;
         for (Entry<Long, NearestPrototypesOperator.PrototypeTuple> entry : inputModel.getSelectedPairs().entrySet()) {
             Long pair = entry.getKey();
             if (pairsMap.containsKey(pair)) {
@@ -115,8 +166,37 @@ public class BatchLoopOperator extends OperatorChain {
             PredictionModel model = predictionModelInnerSourcePort.getData(PredictionModel.class);
             modelsMap.put(pair, model);
         }
+    }
+    //</editor-fold>
 
-        PredictionModel finalModel = new PrototypesEnsemblePredictionModel(inputModel, modelsMap, exampleSet, ExampleSetUtilities.SetsCompareOption.ALLOW_SUPERSET, ExampleSetUtilities.TypesCompareOption.ALLOW_SAME_PARENTS);
+    //<editor-fold desc="Delivery stage" defaultState="collapsed" >
+
+    /**
+     * Method run at the end, delivers created model from operator
+     */
+    private void deliver() {
+        PredictionModel finalModel = new PrototypesEnsemblePredictionModel(inputModel, modelsMap, exampleSet,
+                ExampleSetUtilities.SetsCompareOption.ALLOW_SUPERSET, ExampleSetUtilities.TypesCompareOption.ALLOW_SAME_PARENTS);
         finalModelOutputPort.deliver(finalModel);
     }
+    //</editor-fold>
+
+    //<editor-fold desc="Operator methods" defaultState="collapsed" >
+
+    /**
+     * Main computation method. It takes input example set, generates subsets based
+     * on batch attribute and in a loop each batch is delivered to the subProcess
+     * After each subProcess execution all prediction models are collected in a map,
+     * and returned as a prediction model.
+     *
+     * @throws OperatorException
+     */
+    @Override
+    public void doWork() throws OperatorException {
+        setup();
+        mapPairs();
+        trainExperts();
+        deliver();
+    }
+    //</editor-fold>
 }
